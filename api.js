4b2085e6d5cf2b7e35fef5835faea87b63ab9383
Camila Cristina <criscamila09@gmail.com>
2022-05-17 12:22:38 -0300

    responsividade

--- api.js
+++ api.js
@@ -1,9 +1,11 @@
 // Substitua o CHAVE_DA_API com a chave gerada no site.
 
-export const API_KEY = 'api_key=023351e022928c049941ed91a0a6b835';
+export const API_KEY = 'api_key=023351e022928c049941ed91a0a6b835'
 
-export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/'
 
-export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500'
 
-export const language = 'language=pt-BR';
+export const language = 'language=pt-BR'
+
+
--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-//import { API_KEY, BASE_URL, IMG_URL, language } from './api.js'
-
-const API_KEY = 'api_key=023351e022928c049941ed91a0a6b835'
-
-const BASE_URL = 'https://api.themoviedb.org/3/movie/'
-
-const IMG_URL = 'https://image.tmdb.org/t/p/w500'
-
-const language = 'language=pt-BR'
+import { API_KEY, BASE_URL, IMG_URL, language } from './api.js'
 
 //https://api.themoviedb.org/3/movie/524434?api_key=023351e022928c049941ed91a0a6b835&language=pt-BR
 
@@ -16,11 +8,6 @@ function getMovie() {
   fetch(url)
     .then(res => res.json())
     .then(data => {
-      console.log(movieId)
-      console.log(url)
-      console.log(data)
-      console.log(data.vote_average)
-
       displayMovie(data)
 
       const movieBox = document.getElementById('movie-box')
